bd0f9cf99001ce84812581e40e154dda1d7375e9
Vlad Stohnii <vladislav.stohnii@gmail.com>
2025-06-16 19:45:31 +0300

    +

---
+++ testtestesttest/testtesttest1/testtesttesttest2/script.js
@@ -0,0 +1,28 @@
+// Vulnerable JavaScript Example
+
+// 1. Cross-Site Scripting (XSS)
+function displayUserInput() {
+    var userInput = document.getElementById('userInput').value;
+    // Directly inserting user input into the DOM without sanitization
+    document.getElementById('output').innerHTML = userInput;
+}
+
+// 2. Insecure use of eval
+function executeUserScript() {
+    var userScript = document.getElementById('userScript').value;
+    // Using eval to execute user-provided script
+    eval(userScript);
+}
+
+// 3. Unsecured AJAX request
+function loadUserData() {
+    var xhr = new XMLHttpRequest();
+    // Using HTTP instead of HTTPS and no proper handling of CORS
+    xhr.open('GET', 'http://example.com/userdata', true);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            document.getElementById('ajaxOutput').innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
