98a478921597512fb70308bc3f49aa053ff0e9d9
Vlad Stohnii <vladislav.stohnii@gmail.com>
2025-06-18 21:24:07 +0300

    +

---
+++ new-vuln.js
@@ -0,0 +1,6 @@
+// 1. Cross-Site Scripting (XSS)
+function displayUserInput() {
+    var userInput = document.getElementById('userInput').value;
+    // Directly inserting user input into the DOM without sanitization
+    document.getElementById('output').innerHTML = userInput;
+}
