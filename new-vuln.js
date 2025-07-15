2fc1c3a4c1a2108550b634421109f8f66ef0c3d1
Vlad Stohnii <vladislav.stohnii@gmail.com>
2025-07-15 23:08:16 +0300

    +

--- new-vuln.js
+++ new-vuln.js
@@ -4,3 +4,21 @@ function executeUserScript() {
     // Using eval to execute user-provided script
     eval(userScript);
 }
+// 2. Insecure use of eval
+function executeUserScript() {
+    var userScript = document.getElementById('userScript').value;
+    // Using eval to execute user-provided script
+    eval(userScript);
+}
+// 2. Insecure use of eval
+function executeUserScript() {
+    var userScript = document.getElementById('userScript').value;
+    // Using eval to execute user-provided script
+    eval(userScript);
+}
+// 2. Insecure use of eval
+function executeUserScript() {
+    var userScript = document.getElementById('userScript').value;
+    // Using eval to execute user-provided script
+    eval(userScript);
+}
