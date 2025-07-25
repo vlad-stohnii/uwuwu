ffa95092513efcc982bd3a6afdd06a6d356a6e9c
Vlad Stohnii <vladislav.stohnii@gmail.com>
2025-07-25 23:20:10 +0300

    +

---
+++ hello.ts
@@ -0,0 +1,12 @@
+// 2. Insecure use of eval
+function executeUserScript() {
+    var userScript = document.getElementById('userScript').value;
+    // Using eval to execute user-provided script
+    eval(userScript);
+}
+// 2. Insecure use of eval
+function executeUserScript() {
+    var userScript = document.getElementById('userScript').value;
+    // Using eval to execute user-provided script
+    eval(userScript);
+}
