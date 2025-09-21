efa27dd45e5b0b909bd672ae249c5dcb9149f0cb
ZeroPath <noreply@zeropath.com>
2025-09-21 21:06:35 +0000

    fix: remove eval to prevent XSS vulnerability in user script execution

--- testtestesttest/testtesttest1/testtesttesttest2/script.js
+++ testtestesttest/testtesttest1/testtesttesttest2/script.js
@@ -10,8 +10,8 @@ function displayUserInput() {
 // 2. Insecure use of eval
 function executeUserScript() {
     var userScript = document.getElementById('userScript').value;
-    // Using eval to execute user-provided script
-    eval(userScript);
+    // Removed eval to prevent execution of attacker-controlled scripts
+    console.warn('Execution of user scripts is disabled for security reasons.');
 }
 
 // 3. Unsecured AJAX request
